699fcadc8adde582d2d1d8c7d56ffee1f46060e0
rom1313 <romain.gavin@outlook.fr>
2023-03-23 23:39:54 +0100

    Added my project

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 // todo -------------------------- IMPORTS -----------------------------------
 
 import { changerpage, blockpixelfunc } from "./modules/fonctions.js";
@@ -13,98 +11,75 @@ export let pageactuelle = "acceuil";
 export let contenupagehtml = document.querySelector("#contenupagehtml");
 
 export let countfleche = {
-    value: 0
+  value: 0,
 };
-
 
 // todo -------------------------- EVENTS -----------------------------------
 
 // todo--------- events menu
 genius.event("#acceuil", "click", () => {
-    changerpage("acceuil", contenupagehtml);
-
+  changerpage("acceuil", contenupagehtml);
 });
 
 genius.event("#articles", "click", () => {
-
-    pageactuelle = "articles";
-    changerpage("articles", contenupagehtml);
-
-
-
+  pageactuelle = "articles";
+  changerpage("articles", contenupagehtml);
 });
 genius.event("#contact", "click", () => {
-    pageactuelle = "contact";
-    changerpage("contact", contenupagehtml);
+  pageactuelle = "contact";
+  changerpage("contact", contenupagehtml);
 
-    // todo-------- mail contact
-    genius.event("#mailcopie", "click", (e) => {
-        son.boutton.volume = 0.1;
-        son.boutton2.play();
+  // todo-------- mail contact
+  genius.event("#mailcopie", "click", (e) => {
+    son.boutton.volume = 0.1;
+    son.boutton2.play();
 
-        let p = genius.element("p", "#textecopie");
-        genius.textContent("copié", "#textecopie");
-        p.style.left = e.clientX + 20 + "px";
-        p.style.top = e.clientY - 20 + "px";
-        genius.timeursecondes(0.5, () => {
-            p.remove();
-        });
-        document.querySelector("#telcopie").src = "./ressources/img/copie.png";
-        e.target.src = "./ressources/img/copie2.png";
-        navigator.clipboard.writeText("romain.gavin@outlook.fr");
+    let p = genius.element("p", "#textecopie");
+    genius.textContent("copié", "#textecopie");
+    p.style.left = e.clientX + 20 + "px";
+    p.style.top = e.clientY - 20 + "px";
+    genius.timeursecondes(0.5, () => {
+      p.remove();
     });
-    // todo-------- tel contact
-    genius.event("#telcopie", "click", (e) => {
-        son.boutton.volume = 0.1;
-        son.boutton2.play();
-        let p = genius.element("p", "#textecopie");
-        genius.textContent("copié", "#textecopie");
-        p.style.left = e.clientX + 20 + "px";
-        p.style.top = e.clientY - 20 + "px";
-        genius.timeursecondes(0.5, () => {
-            p.remove();
-        });
-        document.querySelector("#mailcopie").src = "./ressources/img/copie.png";
-        e.target.src = "./ressources/img/copie2.png";
-        navigator.clipboard.writeText("0751133562");
-
+    document.querySelector("#telcopie").src = "./ressources/img/copie.png";
+    e.target.src = "./ressources/img/copie2.png";
+    navigator.clipboard.writeText("romain.gavin@outlook.fr");
+  });
+  // todo-------- tel contact
+  genius.event("#telcopie", "click", (e) => {
+    son.boutton.volume = 0.1;
+    son.boutton2.play();
+    let p = genius.element("p", "#textecopie");
+    genius.textContent("copié", "#textecopie");
+    p.style.left = e.clientX + 20 + "px";
+    p.style.top = e.clientY - 20 + "px";
+    genius.timeursecondes(0.5, () => {
+      p.remove();
     });
+    document.querySelector("#mailcopie").src = "./ressources/img/copie.png";
+    e.target.src = "./ressources/img/copie2.png";
+    navigator.clipboard.writeText("0751133562");
+  });
 });
 // todo-------- Boutton Creation
 genius.event("#creations", "click", () => {
-    son.boutton.volume = 0.1;
-    son.boutton.play();
-    changerpage("creations", contenupagehtml);
-    pageactuelle = "creations";
-    countfleche.value = 0;
-    blockpixelfunc();
-
-
-
+  son.boutton.volume = 0.1;
+  son.boutton.play();
+  changerpage("creations", contenupagehtml);
+  pageactuelle = "creations";
+  countfleche.value = 0;
+  blockpixelfunc();
 });
-
-
-
-
-
 
 // todo-------- events genius powered
 
-
-
 // todo -------------------------- EVENTS GLOBAL -----------------------------------
 
-
 onload = () => {
-
-    particlesJS.load('particles', 'json.json');
-
-
-
-
+  particlesJS.load("particles", "json.json");
 };
 oncontextmenu = () => {
-    return false;
+  return false;
 };
 /* document.onkeydown = function (e) {
  
@@ -132,12 +107,12 @@ oncontextmenu = () => {
 // todo -------------------------- EVENTS SON -----------------------------------
 
 let li = document.querySelectorAll("li");
-li.forEach(element => {
-    /* console.log(element.nodeName); */
-    element.addEventListener("click", function () {
-        son.boutton.volume = 0.1;
-        son.boutton.play();
-    });
+li.forEach((element) => {
+  /* console.log(element.nodeName); */
+  element.addEventListener("click", function () {
+    son.boutton.volume = 0.1;
+    son.boutton.play();
+
+    console.log("tt");
+  });
 });
-
-
--- server.js
+++ server.js
@@ -5,26 +5,20 @@ genius.serveurStatique("./");
 genius.serveurStatique("./ressources");
 genius.serveurStatique("./ressources/autrespages/maquettemode");
 
-
 genius.serveur(13000, () => {
-    console.log('Hey !');
-
+  console.log("Hey !");
 });
 
 genius.serveurRequeteGET("/", (req, res) => {
-    console.log('Requête sur / !');
-    res.sendFile('./index.html', {
-        root: __dirname
-    });
+  console.log("Requête sur / !");
+  res.sendFile("./index.html", {
+    root: __dirname,
+  });
 });
 
 genius.serveurRequeteGET("/mode", (req, res) => {
-    console.log('Requête sur /mode !');
-    res.sendFile('./ressources/autrespages/maquettemode/maquette.html', {
-        root: __dirname
-    });
-})
-
-
-
-
+  console.log("Requête sur /mode !");
+  res.sendFile("./ressources/autrespages/maquettemode/maquette.html", {
+    root: __dirname,
+  });
+});
